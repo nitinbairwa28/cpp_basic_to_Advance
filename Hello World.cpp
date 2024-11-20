ae5bae7637eb791c72340b893a64bc00158df29d
nitinbairwa28 <136908048+nitinbairwa28@users.noreply.github.com>
2024-11-20 22:38:19 +0530

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+// Print Hello World 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+    string x = "Hello";
+    string y = "World";
+    cout<< x +" " + y;
+  
+}
+//Output= Hello World
