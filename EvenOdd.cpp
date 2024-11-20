3566daa831f33b72b1cbcdaa10bd13d6cc7d8b80
nitinbairwa28 <136908048+nitinbairwa28@users.noreply.github.com>
2024-11-20 22:42:34 +0530

    Create EvenOdd.cpp

---
+++ EvenOdd.cpp
@@ -0,0 +1,17 @@
+// print Even,Odd
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number:";
+    cin>>num;
+    if(num%2==0){
+        cout<<"number is even";
+    }else{
+        cout<<"number is odd";
+    }
+    
+}
+/* output= 
+enter the number:13
+number is odd        */
