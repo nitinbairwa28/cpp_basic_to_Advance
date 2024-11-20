3de5a915679c836710dad1a6b0da5a1d7a8f8e51
nitinbairwa28 <136908048+nitinbairwa28@users.noreply.github.com>
2024-11-20 22:40:30 +0530

    Create vote.cpp

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+// Eligible for vote
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age :";
+    cin>>age;
+    if(age>=18){
+        cout<<"eligible to vote";}
+        else{
+            cout<<"not eligible to vote";
+        }
+
+    }
+/*output=
+enter the age :20
+eligible to vote  */
